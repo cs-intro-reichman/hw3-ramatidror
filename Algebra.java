bb2765285714af2cc010dcfd30be5a8113e306c9
Dror Ramati <dror.ramati@post.runi.ac.il>
2024-11-24 12:40:45 +0200

    Update Algebra.java

--- Algebra.java
+++ Algebra.java
@@ -1,27 +1,76 @@
-// Implements algebraic operations and the square root function without using 
-// the Java operations a + b, a - b, a * b, a / b, a % b, and without calling 
-// Math.sqrt. All the functions in this class operate on int values and
-// return int values.
+    public class Algebra {
+        public static void main(String[] args) {
+            System.out.println("givven checks");
+            System.out.println(plus(2,3));   // 2 + 3
+            System.out.println(minus(7,2));  // 7 - 2
+            System.out.println(minus(2,7));  // 2 - 7
+            System.out.println(times(3,4));  // 3 * 4
+            System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
+            System.out.println(pow(5,3));      // 5^3
+            System.out.println(pow(3,5));      // 3^5
+            System.out.println(div(12,3));   // 12 / 3
+            System.out.println(div(5,5));    // 5 / 5
+            System.out.println(div(25,7));   // 25 / 7
+            System.out.println(mod(25,7));   // 25 % 7
+            System.out.println(mod(120,6));  // 120 % 6
+            System.out.println(sqrt(36));
+            System.out.println(sqrt(263169));
+            System.out.println(sqrt(76123));
 
-public class Algebra {
-	public static void main(String args[]) {
-	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
-	}  public static int plus(int a, int b) {
+            System.out.println("plus checks");
+            System.out.println(plus(-1, -1));
+            System.out.println(plus(11, 1));
+            System.out.println(plus(-1, 1));
+            System.out.println(plus(1, -1));
+
+            System.out.println("minus checks");
+            System.out.println(minus(5, 2));
+            System.out.println(minus(5, 10));
+            System.out.println(minus(-5, -2));
+            System.out.println(minus(-5, -6));
+            System.out.println(minus(5, -2));
+            System.out.println(minus(5, 2));
+
+            System.out.println("times checks");
+            System.out.println(times(5, 2));
+            System.out.println(times(5, -2));
+            System.out.println(times(5, 0));
+            System.out.println(times(-5, -2));
+            System.out.println(times(-5, 2));
+
+            System.out.println("pow checks");
+            System.out.println(pow(5, 0));
+            System.out.println(pow(4, 2));
+            System.out.println(pow(-4, 2));
+            System.out.println(pow(-4, 3));
+            System.out.println(pow(4, -2));
+            System.out.println(pow(-4, -2));
+
+            System.out.println("division checks");
+            System.out.println(div(600, 2));
+            System.out.println(div(600, -2));
+            System.out.println(div(-600, -2));
+            System.out.println(div(-600, 2));
+            System.out.println(div(-600, 0));
+            System.out.println(div(600, 0));
+
+            System.out.println("modulo checks");
+            System.out.println(mod(-132, 100));
+            System.out.println(mod(132, 100));
+            System.out.println(mod(132, 132));
+
+            System.out.println("sqrt checks");
+            System.out.println(sqrt(100));
+            System.out.println(sqrt(110));
+            System.out.println(sqrt(-100));
+
+            System.out.println("possitiveValue checks");
+            System.out.println(possitiveValue(25));
+            System.out.println(possitiveValue(0));
+            System.out.println(possitiveValue(-25));
+        }
+
+        public static int plus(int a, int b) {
             int sum = 0;
             if (b >= 0) {
                 for (int i = 0; i <= b; i++) {
@@ -70,7 +119,7 @@ public class Algebra {
             return product;
         }
 
-        public static int pow(int a, int b) {
+        public static int pow (int a, int b) {
             int powResult = a;
             if (b > 0) {
                 for (int i = 0; i < minus(b, 1); i++) {
@@ -78,9 +127,9 @@ public class Algebra {
                 }
             }
             else if (b < 0) {
-                int negativePow = 0;
-                for (int i = 0; i < minus(b, 1); i++) {
-                    negativePow = times(negativePow, a);
+                int negativePow = a;
+                    for (int i = 0; i < minus(b, 1); i++) {
+                        negativePow = times(powResult, a);
                 }
                 powResult = div(1, negativePow);
             }
@@ -136,7 +185,7 @@ public class Algebra {
                 return quotient;
         }
 
-        public static int modulo(int a, int b) {
+        public static int mod(int a, int b) {
             int remainder = minus(a, times(b, (div(a, b))));
             return remainder;
         }
@@ -158,5 +207,6 @@ public class Algebra {
                     System.out.println (" ERROR");
                 }
             return root;
-        }	  	  
-}
+        }
+    }
+
