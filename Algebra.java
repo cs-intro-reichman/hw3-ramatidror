2b1e8b2839c71fa6dc439a80bf28c7a0f872c485
Dror Ramati <dror.ramati@post.runi.ac.il>
2024-11-25 19:28:03 +0200

    Update Algebra.java

--- Algebra.java
+++ Algebra.java
@@ -13,7 +13,7 @@
             System.out.println(div(25,7));   // 25 / 7
             System.out.println(mod(25,7));   // 25 % 7
             System.out.println(mod(120,6));  // 120 % 6
-            System.out.println(sqrt(36));// sqrt 36 = 6
+            System.out.println(sqrt(36));
             System.out.println(sqrt(263169));
             System.out.println(sqrt(76123));
 
